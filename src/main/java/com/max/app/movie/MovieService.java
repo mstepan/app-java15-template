e2348901e658ba450d8a750fb045717bf66f0a0f
Maksym Stepanenko <maksym.stepanenko@opower.com>
2021-01-09 19:13:31 +0200

    Movie service added with Redis/Jedis usage.

--- src/main/java/com/max/app/movie/MovieService.java
+++ src/main/java/com/max/app/movie/MovieService.java
@@ -1,13 +1,14 @@
 package com.max.app.movie;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Transaction;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MovieService implements AutoCloseable {
+public final class MovieService implements AutoCloseable {
+
+    private static final String KEY_FORMAT = "movies:%s:info";
 
     private static final String REDIS_HOST = "localhost";
     private static final int REDIS_PORT = 6379;
@@ -15,42 +16,45 @@ public class MovieService implements AutoCloseable {
     private final Jedis jedis = new Jedis(REDIS_HOST, REDIS_PORT);
 
     public void store(Movie movie) {
-        Transaction transaction = jedis.multi();
-
-        Map<String, String> info = new HashMap<>();
-        info.put("title", movie.getTitle());
-        info.put("description", movie.getDescription());
-        info.put("votes", "0");
-
-        transaction.hset(String.format("movies:%s:info", movie.getId()), info);
-
-        transaction.exec();
+        jedis.hset(key(movie.getId()), toMap(movie));
     }
 
     public Movie findById(String id) {
-        List<String> info = jedis.hmget(String.format("movies:%s:info", id), "title", "description", "votes");
-
-        return new Movie(id, info.get(0), info.get(1), Integer.parseInt(info.get(2)));
+        return toMovie(jedis.hmget(key(id), "id", "title", "description", "votes"));
     }
 
     public void delete(Movie movie) {
-        Transaction tr = jedis.multi();
-
-        tr.del(String.format("movies:%s:info", movie.getId()));
-
-        tr.exec();
+        jedis.del(key(movie.getId()));
     }
 
     public void upVote(Movie movie) {
-        jedis.hincrBy(String.format("movies:%s:info", movie.getId()), "votes", 1);
+        jedis.hincrBy(key(movie.getId()), "votes", 1);
     }
 
     public void downVote(Movie movie) {
-        jedis.hincrBy(String.format("movies:%s:info", movie.getId()), "votes", -1);
+        jedis.hincrBy(key(movie.getId()), "votes", -1);
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         jedis.close();
     }
+
+    private static String key(String id) {
+        return String.format(KEY_FORMAT, id);
+    }
+
+    private static Map<String, String> toMap(Movie movie) {
+        Map<String, String> map = new HashMap<>();
+        map.put("title", movie.getTitle());
+        map.put("description", movie.getDescription());
+        map.put("votes", String.valueOf(movie.getVotes()));
+        return map;
+    }
+
+    private static Movie toMovie(List<String> data) {
+        assert data.size() == 4 : "Incorrect hash length from Redis";
+        return new Movie(data.get(0), data.get(1), data.get(2), Integer.parseInt(data.get(3)));
+    }
+
 }
