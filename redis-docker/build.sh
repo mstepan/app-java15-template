f2bcd5f45b56bccc44d8d28bf39caee4094c6c76
Maksym Stepanenko <maksym.stepanenko@opower.com>
2021-01-16 17:18:10 +0200

    All Redis/Jedis moved to a separate project

--- redis-docker/build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker build -t com.max.redis:6.0 -f Dockerfile .
--- src/main/java/com/max/app/redis/point/XYPoint.java
+++
@@ -1,9 +0,0 @@
-package com.max.app.redis.point;
-
-public record XYPoint(String id, int x, int y) {
-
-
-    public boolean isValid(){
-        return x == y;
-    }
-}
