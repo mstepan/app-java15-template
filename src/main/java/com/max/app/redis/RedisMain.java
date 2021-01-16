f850b5dbb900d492625a91048f9a740b1f5a5c16
Maksym Stepanenko <maksym.stepanenko@opower.com>
2021-01-16 17:11:04 +0200

    Custom redis docker image added.

--- src/main/java/com/max/app/redis/RedisMain.java
+++ src/main/java/com/max/app/redis/RedisMain.java
@@ -2,30 +2,14 @@ package com.max.app.redis;
 
 import redis.clients.jedis.Jedis;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public final class RedisMain {
 
-    private enum NodeType {
-        MASTER("localhost", 6390),
-        SLAVE("localhost", 6391);
-
-        private final String hostname;
-        private final int port;
-
-        NodeType(String hostname, int port) {
-            this.hostname = hostname;
-            this.port = port;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-        Jedis jedis = new Jedis(NodeType.MASTER.hostname, NodeType.MASTER.port);
-        jedis.auth("611191");
+        Jedis jedis = new Jedis("localhost", 6379);
 
         try {
-            jedis.mset("key1", "222");
+            jedis.select(0);
+            jedis.set("key1", "some value 111");
             System.out.println(jedis.get("key1"));
         }
         finally {
